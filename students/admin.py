1befe869801e020c35aa77d32d70df08117999c2
kylewetzler <45954950+kylewetzler@users.noreply.github.com>
2020-07-05 15:34:35 -0400

    adds student organizations model

--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Major
+from .models import OfferedMajor, StudentOrganization
 
 # Register your models here.
-admin.site.register(Major)
+admin.site.register(OfferedMajor)
+admin.site.register(StudentOrganization)
--- students/models.py
+++ students/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 
-class Major(models.Model):
+class OfferedMajor(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+
+class StudentOrganization(models.Model):
+    name = models.CharField(max_length=400)
